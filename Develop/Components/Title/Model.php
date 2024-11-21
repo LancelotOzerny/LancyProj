332eba2ec55285d77595c47dea1cec2da7df0a72
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-11-21 03:09:35 +0300

    Добавил компонент заголовка

---
+++ Develop/Components/Title/Model.php
@@ -0,0 +1,10 @@
+<?php
+namespace Develop\Components\Title;
+
+class Model extends \Core\Modules\System\Libs\Components\Model
+{
+    public function getData(): array
+    {
+        return [];
+    }
+}
---
+++ Develop/Components/Title/template.php
@@ -0,0 +1,8 @@
+<?php
+/** @var array $componentParams */
+/** @var array $templateParams */
+
+$title = $templateParams['title'] ?? 'Empty Title';
+?>
+
+<h2><?= $title ?></h2>
