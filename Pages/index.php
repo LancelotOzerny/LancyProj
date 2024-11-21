cccdefb177b4dae13d913062c8dc4860754133ea
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-11-21 03:11:00 +0300

    Построил пример скелета страницы

--- Pages/index.php
+++ Pages/index.php
@@ -1,2 +1,23 @@
 <?php
 /** @var \Core\Modules\System\Libs\Page $page */
+
+$page->setParam('title', 'Сайт портфолио');
+
+
+/* HEADER */
+$row = $page->grid->createRow();
+$row->createColumn('Title', ['title' => 'test']);
+
+
+/* About me */
+$row = $page->grid->createRow();
+$row->containerIncluded = true;
+
+
+/* Portfolio Works */
+$row = $page->grid->createRow();
+$row->containerIncluded = true;
+
+
+/* FOOTER */
+$row = $page->grid->createRow();
