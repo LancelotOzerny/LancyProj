15401b2db7040bdfeec8df1621f7f51a7419fb44
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-11-24 00:51:11 +0300

    Добавил заголовок и страницы портфолио, контактов

--- Pages/blog/index.php
+++ Pages/blog/index.php
@@ -2,7 +2,3 @@
 /** @var \Core\Modules\System\Libs\Page $page */
 
 $page->setParam('title', 'Сайт портфолио: Блог');
-
-$page->grid->createContainer()->createRow()->createColumn()->addSelector(new \Core\Modules\System\Libs\Selectors\HtmlCode(<<<"HTML"
-    <h2 style="font-size: 3rem">Blog</h2>
-HTML));
