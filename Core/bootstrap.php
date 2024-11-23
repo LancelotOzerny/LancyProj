1d7e875bd1ff89b6b2c7317da840d8182695eac8
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-11-23 04:19:48 +0300

    Исправил путь к файлу на полный

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -6,17 +6,21 @@ require ROOT_DIR . '/Core/Loader.php';
 spl_autoload_register('Core\Loader::ClassAutoload');
 
 $pageParams = $_SERVER['QUERY_STRING'];
-$pagePath = $_SERVER['SCRIPT_NAME'];
+$pagePath = $_SERVER['REQUEST_URI'];
 
 if (str_ends_with($pagePath, '/'))
 {
     $pagePath .= 'index.php';
 }
 
-if (file_exists($pagePath = ROOT_DIR . "/Pages/$pagePath"))
+if (file_exists($pagePath = ROOT_DIR . '/Pages' . $pagePath))
 {
     $page = \Core\Modules\System\Libs\Page::getInstance();
+
+    $page->includeHeader();
     include $pagePath;
+    $page->includeFooter();
+
     $page->build();
 }
 else
